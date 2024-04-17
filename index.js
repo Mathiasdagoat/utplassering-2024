e37b198c60604fee397543322c778ecebebbd313
Mathias Solsem Austli <mathiassoa@gmail.com>
2024-04-17 12:23:24 +0200

    bokstav gjeting som ikke fungerer

--- index.js
+++ index.js
@@ -9,3 +9,16 @@ socket.on("connect", ()=> {
 socket.on("disconnect", () => {
     console.log(socket.id);
 });
+
+function handleKeyPress(event) {
+    if (event.key === "Enter") {
+        displayText();
+    }
+}
+
+function displayText() {
+var inputText = document.getElementById("inputField").value;
+document.getElementById("output").innerText = inputText;
+}
+
+document.getElementById("inputField").addEventListener("keydown", handleKeyPress);
