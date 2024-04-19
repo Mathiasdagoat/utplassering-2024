9b0d8236c556515ec5f08fce3648f02c88b3d5cb
Mathias Solsem Austli <mathiassoa@gmail.com>
2024-04-19 10:06:58 +0200

    linjer

--- index.js
+++ index.js
@@ -1,5 +1,36 @@
 import{ io } from "socket.io-client";
 
+const canvas = document.getElementById("stickman");
+const ctx = canvas.getContext("2d");
+
+ctx.beginPath();
+ctx.moveTo(10,220);
+ctx.lineTo(150,220);
+ctx.lineWidth = 2;
+ctx.strokeStyle = "green";
+ctx.stroke();
+
+ctx.beginPath();
+ctx.moveTo(60,80);
+ctx.lineTo(60,220);
+ctx.lineWidth = 2;
+ctx.strokeStyle = "green";
+ctx.stroke();
+
+ctx.beginPath();
+ctx.moveTo(170,30);
+ctx.lineTo(60,80);
+ctx.lineWidth = 2;
+ctx.strokeStyle = "green";
+ctx.stroke();
+
+ctx.beginPath();
+ctx.moveTo(170,30);
+ctx.lineTo(170,80);
+ctx.lineWidth = 2;
+ctx.strokeStyle = "brown";
+ctx.stroke();
+
 const socket = io();
 
 socket.on("connect", ()=> {
