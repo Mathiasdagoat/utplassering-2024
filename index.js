3b215fade8037f288b078165ba283bfd8884a3b3
Aleksander Lunde <aleksanderlunde71@gmail.com>
2024-04-22 08:30:33 +0200

    Some new changes

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ import{ io } from "socket.io-client";
 const canvas = document.getElementById("stickman");
 const ctx = canvas.getContext("2d");
 
-let lifeUsed = ("5")
+let lifeUsed = ("10")
 
-if (lifeUsed > 1) {
+if (lifeUsed > 0) {
 ctx.beginPath();
 ctx.moveTo(10,220);
 ctx.lineTo(150,220);
@@ -14,7 +14,7 @@ ctx.strokeStyle = "green";
 ctx.stroke();
 }
 
-if (lifeUsed > 2) {
+if (lifeUsed > 1) {
 ctx.beginPath();
 ctx.moveTo(60,80);
 ctx.lineTo(60,220);
@@ -23,7 +23,7 @@ ctx.strokeStyle = "green";
 ctx.stroke();
 }
 
-if (lifeUsed > 3) {
+if (lifeUsed > 2) {
 ctx.beginPath();
 ctx.moveTo(170,30);
 ctx.lineTo(60,80);
@@ -32,7 +32,7 @@ ctx.strokeStyle = "green";
 ctx.stroke();
 }
 
-if (lifeUsed > 4) {
+if (lifeUsed > 3) {
 ctx.beginPath();
 ctx.moveTo(170,30);
 ctx.lineTo(170,80);
@@ -41,17 +41,28 @@ ctx.strokeStyle = "brown";
 ctx.stroke();
 }
 
-if (lifeUsed > 5) {
+if (lifeUsed > 4) {
 ctx.beginPath();
-ctx.arc(170,100,20,0,2*Math.PI);
+ctx.arc(170,87,7,0,2*Math.PI);
+ctx.strokeStyle = "black";
+ctx.fillStyle = "white";
+ctx.fill();
+ctx.stroke();
+}
+
+if (lifeUsed > 5){
+ctx.beginPath();
+ctx.moveTo(170,95);
+ctx.lineTo(170,124);
+ctx.lineWidth = 2;
 ctx.strokeStyle = "black";
 ctx.stroke();
 }
 
-if (lifeUsed > 6){
+if (lifeUsed > 6) {
 ctx.beginPath();
-ctx.moveTo(170,120);
-ctx.lineTo(170,170);
+ctx.moveTo(170,99);
+ctx.lineTo(160,120);
 ctx.lineWidth = 2;
 ctx.strokeStyle = "black";
 ctx.stroke();
@@ -59,8 +70,8 @@ ctx.stroke();
 
 if (lifeUsed > 7) {
 ctx.beginPath();
-ctx.moveTo(170,125);
-ctx.lineTo(150,150);
+ctx.moveTo(170,99);
+ctx.lineTo(180,120);
 ctx.lineWidth = 2;
 ctx.strokeStyle = "black";
 ctx.stroke();
@@ -68,8 +79,8 @@ ctx.stroke();
 
 if (lifeUsed > 8) {
 ctx.beginPath();
-ctx.moveTo(170,125);
-ctx.lineTo(190,150);
+ctx.moveTo(170,120);
+ctx.lineTo(165,145);
 ctx.lineWidth = 2;
 ctx.strokeStyle = "black";
 ctx.stroke();
@@ -77,23 +88,14 @@ ctx.stroke();
 
 if (lifeUsed > 9) {
 ctx.beginPath();
-ctx.moveTo(170,170);
-ctx.lineTo(140,210);
+ctx.moveTo(170,120);
+ctx.lineTo(175,145);
 ctx.lineWidth = 2;
 ctx.strokeStyle = "black";
 ctx.stroke();
 }
 
-if (lifeUsed == 10) {
-ctx.beginPath();
-ctx.moveTo(170,170);
-ctx.lineTo(200,210);
-ctx.lineWidth = 2;
-ctx.strokeStyle = "black";
-ctx.stroke();
-}
-
-const socket = io();
+const socket = io("http://localhost:3000");
 
 socket.on("connect", ()=> {
     console.log(socket.id);
